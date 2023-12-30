037894147f6553acf7e91bb9bf1e69749a0f4556
bintang <bintangarya2456@gmail.com>
2023-12-30 14:15:21 +0700

    first commit

---
+++ app/Filament/Resources/MahasiswaResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Mahasiswa;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\MahasiswaResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+
+class MahasiswaResource extends Resource
+{
+    protected static ?string $model = Mahasiswa::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nim')->required()->unique(ignorable:fn($record)=>$record),
+                TextInput::make('nama')->required(),
+                TextInput::make('email')->required(),
+                TextInput::make('alamat')->required(),
+                TextInput::make('umur')->required(),
+                FileUpload::make('foto'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nim') -> sortable() -> searchable(),
+                TextColumn::make('nama') -> sortable() -> searchable(),
+                TextColumn::make('email') -> sortable() -> searchable(),
+                TextColumn::make('alamat') -> sortable() -> searchable(),
+                TextColumn::make('umur') -> sortable() -> searchable(),
+                ImageColumn::make('foto')->square(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Filament/Resources/StaffResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Staff;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\StaffResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\StaffResource\RelationManagers;
+
+class StaffResource extends Resource
+{
+    protected static ?string $model = Staff::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nim')->required()->unique(ignorable:fn($record)=>$record),
+                TextInput::make('nama')->required(),
+                TextInput::make('email')->required(),
+                TextInput::make('alamat')->required(),
+                TextInput::make('umur')->required(),
+                FileUpload::make('foto'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nim') -> sortable() -> searchable(),
+                TextColumn::make('nama') -> sortable() -> searchable(),
+                TextColumn::make('email') -> sortable() -> searchable(),
+                TextColumn::make('alamat') -> sortable() -> searchable(),
+                TextColumn::make('umur') -> sortable() -> searchable(),
+                ImageColumn::make('foto')->square(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStaff::route('/'),
+            'create' => Pages\CreateStaff::route('/create'),
+            'edit' => Pages\EditStaff::route('/{record}/edit'),
+        ];
+    }    
+}
